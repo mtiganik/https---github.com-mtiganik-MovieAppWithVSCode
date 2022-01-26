00e12a2380b448a6a4f687dc1e3c49cc17ea1cc1
Mihkel Tiganik <mtiganik@gmail.com>
2022-01-26 19:03:50 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MovieApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
